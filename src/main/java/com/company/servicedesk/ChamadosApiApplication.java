4731ddb9e2832e50d373ff15e868b22d13684ff8
Israel David <israeldiv100@gmail.com>
2026-04-22 00:21:19 -0300

    first commit

---
+++ src/main/java/com/company/servicedesk/ChamadosApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.servicedesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamadosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamadosApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/company/servicedesk/models/UserModel.java
@@ -0,0 +1,30 @@
+package com.company.servicedesk.models;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "Users") //plural to avoid errors with the postgres
+@Table(name = "tb_users")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class UserModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
